0ee965e17c7bfda24c3aaca91517028b424c3c5a
Кирилл <just.another.kirill.ilichev@gmail.com>
2020-02-06 01:20:37 +0300

    Added a IDisposable interface to the Resource class

--- core/Resource.cs
+++ core/Resource.cs
@@ -2,13 +2,50 @@ using System;
 
 namespace SimpleGame.Core
 {
-    public abstract class Resource
+    public abstract class Resource : IDisposable
     {
+        private bool _disposed;
         public SDLApplication OwnerApp { get; }
 
         public Resource(SDLApplication owner)
         {
             OwnerApp = owner;
+        }
+
+        protected void CheckDisposed(string objectName)
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(objectName);
+            }
+        }
+
+        protected virtual void FreeManaged() {}
+        protected virtual void FreeUnmanaged() {}
+
+        private void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                FreeManaged();
+            }
+
+            FreeUnmanaged();
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Resource()
+        {
+            Dispose(false);
         }
     }
 }
