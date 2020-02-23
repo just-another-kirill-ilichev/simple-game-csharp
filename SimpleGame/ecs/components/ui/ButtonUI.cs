3199e2df5e3bbf19e76bf70d98394fb6cd912495
Кирилл <just.another.kirill.ilichev@gmail.com>
2020-02-23 21:35:24 +0300

    Implemented some ui features

--- SimpleGame/ecs/components/ui/ButtonUI.cs
+++ SimpleGame/ecs/components/ui/ButtonUI.cs
@@ -2,13 +2,16 @@ using System.Drawing;
 
 namespace SimpleGame.ECS.Components.UI
 {
+    public enum ButtonState { Pressed, Released, Hovered }
+
     public class ButtonUI : TextUI
     {
         public Color HoveredBackgroundColor { get; set; }
-        public string HoveredBackgroundTextureResourceName { get; set; }
+        public string HoveredBackgroundTextureResourceRef { get; set; }
         public Color HoveredTextColor { get; set; }
         public Color PressedBackgroundColor { get; set; }
-        public string PressedBackgroundTextureResourceName { get; set; }
+        public string PressedBackgroundTextureResourceRef { get; set; }
         public Color PressedTextColor { get; set; }
+        public ButtonState State { get; set; }
     }
 }
--- SimpleGame/ecs/components/ui/TextUI.cs
+++ SimpleGame/ecs/components/ui/TextUI.cs
@@ -5,8 +5,9 @@ namespace SimpleGame.ECS.Components.UI
     public class TextUI : Component
     {
         public string Text { get; set; }
+        public string FontResourceRef { get; set; }
         public Color BackgroundColor { get; set; }
-        public string BackgroundTextureResourceName { get; set; }
+        public string BackgroundTextureResourceRef { get; set; }
         public Color TextColor { get; set; }
     }
 }
