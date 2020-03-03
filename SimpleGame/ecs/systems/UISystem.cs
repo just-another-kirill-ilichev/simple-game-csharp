8b1f938e77c0310c1c383e000ddb3c0f63259cfd
Кирилл <just.another.kirill.ilichev@gmail.com>
2020-03-03 20:18:52 +0300

    Some ui system updates

--- SimpleGame/ecs/systems/UISystem.cs
+++ SimpleGame/ecs/systems/UISystem.cs
@@ -5,14 +5,39 @@ using SimpleGame.Core;
 using SimpleGame.ECS.Components;
 using SimpleGame.ECS.Components.UI;
 
-namespace SimpleGame.ECS.Systems{
+namespace SimpleGame.ECS.Systems
+{
     public class UISystem : SystemBase
     {
-        private Dictionary<int, string> _textureCache;
+        private Dictionary<int, string> _renderedFontTextureCache;
 
         public UISystem(SDLApplication owner) : base(owner)
         {
+            _renderedFontTextureCache = new Dictionary<int, string>();
+        }
 
+        public void UpdateRenderedFontTextureCache(int entity)
+        {
+            var component = OwnerApp.EntityManager.GetComponent<TextUI>(entity);
+            var position = OwnerApp.EntityManager.GetComponent<TransformComponent>(entity);
+            var font = OwnerApp.ResourceManager.Get<Font>(component.FontResourceRef);
+
+            using (var surf = font.Print(component.Text, component.TextColor))
+            {
+                var texture = new Texture(OwnerApp, surf);
+
+                if (_renderedFontTextureCache.ContainsKey(entity))
+                {
+                    var res = OwnerApp.ResourceManager.Get<Texture>(_renderedFontTextureCache[entity]);
+                    res.Dispose();
+                    res = texture;
+                }
+                else
+                {
+                    _renderedFontTextureCache[entity] = "textureCache" + entity.ToString();
+                    OwnerApp.ResourceManager.Add(_renderedFontTextureCache[entity], texture);
+                }
+            }
         }
 
         public override void Redraw()
@@ -25,7 +50,7 @@ namespace SimpleGame.ECS.Systems{
                 .WithComponent<TextUI>()
                 .ToArray();
 
-            
+
             foreach (var entity in texts)
             {
                 var component = OwnerApp.EntityManager.GetComponent<TextUI>(entity);
@@ -34,12 +59,14 @@ namespace SimpleGame.ECS.Systems{
 
                 var font = OwnerApp.ResourceManager.Get<Font>(component.FontResourceRef);
 
-                using (var surf = font.Print(component.Text, component.TextColor))
-                using (var texture = new Texture(OwnerApp, surf))
+                if (_renderedFontTextureCache.ContainsKey(entity))
                 {
-                    texture.Draw((int)position.X, (int)position.Y); // TODO cache textures
+                    OwnerApp.ResourceManager.Get<Texture>(_renderedFontTextureCache[entity]).Draw((int)position.X, (int)position.Y);
                 }
-                
+                else
+                {
+                    UpdateRenderedFontTextureCache(entity);
+                }
             }
         }
     }
