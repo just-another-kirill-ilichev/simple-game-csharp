37cba5872aa619c6842e907a45b519f5cacfaa53
Kirill Ilichev <just.another.kirill.ilichev@gmail.com>
2020-03-25 17:11:05 +0300

    Added scale in TransformComponent

--- SimpleGame/ecs/components/TransformComponent.cs
+++ SimpleGame/ecs/components/TransformComponent.cs
@@ -13,8 +13,13 @@ namespace SimpleGame.ECS.Components
         public float RotationY { get; set; }
         public float RotationZ { get; set; }
 
+        public float ScaleX { get; set; } = 0.1f;
+        public float ScaleY { get; set; } = 0.1f;
+        public float ScaleZ { get; set; } = 0.1f;
+
         [JsonIgnore]
-        public Matrix4 Transform => Matrix4.CreateRotationX(RotationX) *
+        public Matrix4 Transform => Matrix4.CreateScale(ScaleX, ScaleY, ScaleZ) *
+                Matrix4.CreateRotationX(RotationX) *
                 Matrix4.CreateRotationY(RotationY) *
                 Matrix4.CreateRotationZ(RotationZ) *
                 Matrix4.CreateTranslation(X, Y, Z);
